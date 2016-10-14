17b042ddc5f75a8848d520281345d563613da864
dekon91 <tkhovanich@hotmail.com>
2016-10-14 16:38:07 +0700

    Update line.py

--- line.py
+++ line.py
@@ -1,49 +1,47 @@
-from flask import Flask, request
-import json
-import requests
- 
+import os
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
 app = Flask(__name__)
- 
-@app.route('/')
-def index():
-    return "Hello World!"
-# ส่วน callback สำหรับ Webhook
-@app.route('/callback', methods=['POST'])
+
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+
+
+@app.route("/callback", methods=['POST'])
 def callback():
-    json_line = request.get_json()
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    #id=[d['replyToken'] for d in user][0]
-    #print(json_line)
-    print("ผู้ใช้：",user)
-    sendText(user,'งง') # ส่งข้อความ งง
-    return '',200
- 
-def sendText(user, text):
-    LINE_API = 'https://api.line.me/v2/bot/message/reply'
-    Authorization = 'Bearer ' + os.getenv('LINE_KEY')
-    #Authorization = os.environ['LINE_KEY'] # ใส่ ENTER_ACCESS_TOKEN เข้าไป
- 
-    #headers = {
-    #    'Content-Type': 'application/json; charset=UTF-8',
-    #    'Authorization':Authorization
-    #}
-    headers = {}
-    headers['Content-Type'] = 'application/json; charset=UTF-8'
-    headers['Authorization'] = Authorization
- 
-    data = json.dumps({
-        "replyToken":user,
-        "messages":[{
-            "type":"text",
-            "text":text
-        }]
-    })
- 
-    #print("ข้อมูล：",data)
-    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
-    #print(r.text)
- 
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    #line_bot_api.reply_message(
+    #    event.reply_token,
+    #    TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(reply_token, TextSendMessage(text='Hello World!'))
+    
+
 if __name__ == '__main__':
      app.run(debug=True)
